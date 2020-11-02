5b3e9433035861a6163bce0e65fddce28a19dff6
maradoxbat <54273303+maradoxbat@users.noreply.github.com>
2020-11-02 23:41:11 +0200

    Update setup.sh
    install npm and apt update

--- setup.sh
+++ setup.sh
@@ -1,37 +1,42 @@
 #!/bin/bash
-NUMBER=7
+NUMBER=8
 
 echo "Welecome to the YStem and Chess Setup Installer"
 echo "-----------------------------------------------"
 echo ""
-echo "(1/$NUMBER) Installing Node.js..."
-sudo apt install -y nodejs > /dev/null
+echo "(1/$NUMBER) Updating..."
+sudo apt update
 echo "::Done"
 
-echo "(2/$NUMBER) Installing Angular CLI..."
+echo "(2/$NUMBER) Installing Node.js..."
+sudo apt install -y nodejs > /dev/null
+sudo apt install -y npm > /dev/null
+echo "::Done"
+
+echo "(3/$NUMBER) Installing Angular CLI..."
 sudo npm install -g @angular/cli > /dev/null
 sudo npm install -g @angular-devkit/build-angular > /dev/null
 echo "::Done"
 
-echo "(3/$NUMBER) Installing php"
+echo "(4/$NUMBER) Installing php"
 sudo apt install -y php > /dev/null
 echo "::Done"
 
-echo "(4/$NUMBER) Installing mongodb driver"
+echo "(5/$NUMBER) Installing mongodb driver"
 sudo apt install -y php-pear php-dev > /dev/null
 sudo pecl install mongodb > /dev/null
 echo "extension=mongodb.so" | sudo tee -a $(php -i | grep "Loaded Configuration File" | awk '{print $5}') > /dev/null
 echo "::Done"
 
-echo "(5/$NUMBER) Installing nodemon"
+echo "(6/$NUMBER) Installing nodemon"
 sudo npm isntall -g nodemon > /dev/null
 echo "::Done"
 
-echo "(6/$NUMBER) Installing express"
+echo "(7/$NUMBER) Installing express"
 sudo npm install -g express > /dev/null
 echo "::Done"
 
-echo "(7/$NUMBER) Installing socket.io"
+echo "(8/$NUMBER) Installing socket.io"
 sudo npm install -g socket.io > /dev/null
 echo "::Done"
 
